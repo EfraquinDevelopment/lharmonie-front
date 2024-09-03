75ff27b0346a2de293a2fc3d0b8c1e1197a1a141
EfraquinDevelopment <efraquindevelopment@gmail.com>
2024-09-02 21:24:01 -0300

    fix

--- src/components/cart.tsx
+++ src/components/cart.tsx
@@ -1,6 +1,8 @@
 import { ShoppingCartOutlined } from "@ant-design/icons";
 import useDrawer from "@/hooks/useDrawer";
-import { Button, Drawer } from "antd";
+import { Button, Drawer, Badge } from "antd";
+import { useContext } from "react";
+import CartContext from "@/context/cart-context";
 
 type Props = {
   className?: string;
@@ -8,16 +10,25 @@ type Props = {
 
 const Cart = ({ className }: Props) => {
   const { isOpen, openDrawer, closeDrawer } = useDrawer();
+  const cartContext = useContext(CartContext);
+
+  if (!cartContext) {
+    throw new Error("useCart must be used within a CartProvider");
+  }
+
+  const { cartItems } = cartContext;
 
   return (
     <>
-      <Button
-        icon={
-          <ShoppingCartOutlined className="text-xl text-lharmonie-secondary" />
-        }
-        onClick={openDrawer}
-        className={className}
-      />
+      <Badge count={cartItems.length}>
+        <Button
+          icon={
+            <ShoppingCartOutlined className="text-xl text-lharmonie-secondary" />
+          }
+          onClick={openDrawer}
+          className={className}
+        />
+      </Badge>
 
       <Drawer
         title={<h2 className="font-medium text-xl">Cart</h2>}
